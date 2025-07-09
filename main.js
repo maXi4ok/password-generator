c8185badd4f8172784c4531263fa05cd4afff883
maXi4ok <mftank22@gmail.com>
2025-07-09 17:26:05 +0300

    added buttons

--- main.js
+++ main.js
@@ -1,32 +1,40 @@
 let charsForPassword = 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!#$%^&*_';
 let charsArray = [...charsForPassword]
-
+// -----------random-Int-Function----------------
 function getRandomInt(max) {
     return Math.floor(Math.random() * max)
 }
-
-console.log(charsArray);
-
+// ---------------------------------
+// ------------all-lets-that-is-needed----------
+let length = +prompt('enter password length');
 let uniquePassword = new Set();
 let password = '';
-function uniquePasswordGenerator(length) {        
-    while (uniquePassword.size < length + 1) {
+let unique = document.querySelector('.unique');
+let common = document.querySelector('.common');
+// ------functions-for-passwords-------------------------------
+function uniquePasswordGenerator() {        
+    while (uniquePassword.size < length) {
         let randomIndex = getRandomInt(charsArray.length);
         let char = charsArray[randomIndex];
         uniquePassword.add(char); 
     }
-        return uniquePassword;
+    let uniquePasswordArray = Array.from(uniquePassword);
+    let uniquePasswordString = uniquePasswordArray.join('')
+    unique.innerText = `your unique password: ${uniquePasswordString}`;
+    return uniquePasswordString;
 }
-function passwordGenerator(length) {
-    for (let i = 0; i < length + 1; i++) {
+function passwordGenerator() {
+    for (let i = 0; i < length; i++) {
         let char = charsArray[getRandomInt(charsArray.length)];
         password = password + char;
     }
+    common.innerText = `your password: ${password}`
     return password;
 
 }
+// -----------------------------------------------------------------
 
-console.log(uniquePasswordGenerator(8));
+
 
 
 
